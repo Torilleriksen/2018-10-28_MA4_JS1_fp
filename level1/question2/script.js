5fbb61a21a95c1313d14e1dba71ac21bd7d1ab62
Torilleriksen <36129734+Torilleriksen@users.noreply.github.com>
2018-10-29 21:40:51 +0200

    Update script.js

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,6 +1,5 @@
 
 //Access json file 
-
 var request = new XMLHttpRequest();
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos', true);
 request.responseType = 'json';
@@ -12,4 +11,26 @@ request.onload = function() {
   notesTodo(toDo); 
   }
   
+//Displaying TODO notes in cards
+function notesTodo(jsonObj) {
   
+  for (var i = 0; i < toDo.lenght; i++) {
+  var idNr = document.createElement('h1');
+  var noteNr = document.createElement('h2');
+  var whatTodo = document.createElement('p');
+  var status = document.createElement('p');
+    
+  idNr.textContent = 'User: ' + toDo[i].userId;
+  noteNr.textContent = 'Number: ' + toDo[i].id;
+  whatTodo.textContent = 'Task: ' + toDo[i].title;
+  status.textContent = 'Status' + toDo[i].completed; 
+     }
+
+section.appendChild(idNr);
+section.appendChild(noteNr);
+section.appendChild(whatTodo);  
+section.appendChild(status);
+
+
+  }
+}
